f5cdd30459ab84530b3426f7b167fb1e4079eb28
Daniel Pali <danielpali@Daniels-MBP.home>
2019-09-23 11:35:04 -0400

    "fixed some errors"

--- jqueryStyle.js
+++ jqueryStyle.js
@@ -1,69 +1,80 @@
-// not in use
+'use strict'
 
 
+//documentation page below  ↓↓↓
+//https://www.weatherbit.io/api/airquality-current
 
-/*!
- * jQuery.anchorScroll jQuery Plugin v1.0
- *
- * Author: Virgiliu Diaconu
- * http://www.virgiliu.com
- * Licensed under the MIT license.
- */
-;
-/*(function($, window, document, undefined) {
-    'use strict';
-    $(".anchorScroll") = function(el, options) {
-        var base = this;
+// "https://api.weatherbit.io/v2.0/current"
 
-        // Access to jQuery and DOM versions of element
-        base.$el = $(el);
-        base.el = el;
+// apikey for weatherbit: "8103110ef30f43aba760f8b8e90d3ee8"
 
-        base.init = function() {
-            base.options = $.extend({}, $.anchorScroll.defaultOptions, options);
-        };
+const apiKey = "8103110ef30f43aba760f8b8e90d3ee8"
 
-        // On click
-        base.$el.click(function(e) {
-            e.preventDefault();
-            if ($(e.target).closest('a').length && $(base.el.hash).length) {
-                var targetPos = $(base.el.hash).offset().top - base.options.offsetTop,
-                    classTo = (base.$el.data("classTo") === "this") ? base.el : base.$el.data("classTo"),
-                    onScroll = base.$el.data("onScroll"),
-                    scrollEnd = base.$el.data("scrollEnd");
-                // Callback scroll start
-                if (typeof base.options.scrollStart === 'function') {
-                    base.options.scrollStart.call(el);
-                }
-                // Add class to element on scroll
-                $(classTo).addClass(onScroll).removeClass(scrollEnd);
-                // Smooth scroll
-                $('html,body').animate({
-                    scrollTop: targetPos
-                }, base.options.scrollSpeed).promise().done(function() {
-                    // On animation complete
-                    $(classTo).addClass(scrollEnd).removeClass(onScroll);
-                    // Callback on scroll end
-                    if (typeof base.options.scrollEnd === 'function') {
-                        base.options.scrollEnd.call(el);
-                    }
-                });
+const searchUrl = "https://api.weatherbit.io/v2.0/current/airquality"
+
+
+function formatQueryParams(params) {
+    const queryItems = $.param(params);
+    return queryItems;
+}
+function displayResults(responseJson){
+    console.log(responseJson)
+    $('#js-aqiResults').empty();
+    $('#js-aqiResults').append(`
+    <h2>${responseJson.city_name}</h2>`)
+
+    for (let i = 0; i < responseJson.data.length; i++){
+    //$('#js-aqiResults').append(
+        //`<p>${responseJson.data[i].aqi}</p>`)
+        let aqiNum = `${responseJson.data[i].aqi}`
+        for (let i = 0; i < responseJson.data.length; i++) {
+            const infoObj = responseJson.data[i]
+            for (let propName in infoObj) {
+                $('#js-aqiResults').append(`
+                    <p><span>${propName}</span> ${infoObj[propName]}</p>`
+                )
             }
-        });
+        $("#js-wordResponse").empty()
+    }
+}
 
-        // Run initializer
-        base.init();
+
+function particles(query){
+    const params = {
+        postal_code: query,
+        key: apiKey,
+        //language: 'en'
+        //"content-type": "application/json; charset=utf-8"
     };
 
-    $.anchorScroll.defaultOptions = {
-        scrollSpeed: 800,
-        offsetTop: 0
-    };
+ const queryString = formatQueryParams(params)
+ const url = searchUrl + '?' + queryString
 
-    $.fn.anchorScroll = function(options) {
-        return this.each(function() {
-            (new $.anchorScroll(this, options));
-        });
-    };
+    console.log(url)
 
-})(jQuery, window, document);*/
+
+ $('#js-aqiResults').html('<p>Checking particles in the air...</p>')
+    fetch(url)
+     .then(response => {
+        if (response.ok) {
+            return response.json();
+        }
+            throw new Error(response.statusText);
+        })
+     .then(responseJson => displayResults(responseJson))
+     .catch(err => {
+        $('#js-aqiResults').empty()
+        $('#js-error').text(`something went wrong: ${err.message}`);
+    })}
+}
+
+
+function watchFormSecond(){
+    $("#findDetailed-btn").on("click", event =>{
+        event.preventDefault()
+        const searchTerm = $("#zip-code").val();
+    particles(searchTerm);
+    })
+}
+
+$(watchFormSecond)
