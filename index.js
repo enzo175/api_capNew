a25b574080ea82d295444cd9ac86e69dace3b778
Daniel Pali <danielpali@Daniels-MBP.home>
2019-09-24 14:52:06 -0400

    "UI additions"

--- index.js
+++ index.js
@@ -58,10 +58,13 @@ function displayResults(responseJson) {
             barIndex = 2
             $("#js-wordResponse").html("Unhealthy")
         }
-        // TODO add the 4th
+        else if (aqiNum >= 150, aqiNum < 200){
+            barIndex = 3
+            $("#js-wordResponse").html('Really Unhealthy')
+        }
         else {
             barIndex = 4
-            $("#js-wordResponse").html('Very Unhealthy')
+            $("#js-wordResponse").html('Stay Inside')
         }
     }
     updateBarColor(barIndex)
@@ -105,19 +108,39 @@ function formEffect() {
 
 function sliding() {
     $(".container").animate({ width: "250px" }, 2000)
+    let flag = true;
     $('form').submit(event => {
         $(".result-wrapper").animate({ width: "250px" }, 2000)
         $('.hidden').fadeIn(2000)
+        if (flag){
         if (window.innerWidth > 700) {
             // TODO animate with CSS transitions https://repl.it/@TudorIlisoi/CSSTransitions
-            // $('canvas').animate({ marginTop: "-=500px" }, 4000);
-        }
+             $('canvas').animate({ marginTop: "-=450px", maxHeight: "450px" }, 3500);
+             flag = false;
+        }}
     })
 }
 
+function aboutUsButton(){
+    $('#js-aboutUs').on('click', event =>{
+        event.preventDefault();
+        $('.js-whole-wrapper').empty();
+        $('.js-whole-wrapper').append(`
+        <section class="us"> <h2> This page is dedicated to keeping you informed on
+        the latest Air quality information so you can always be in the know about 
+        what is going in and out of your lungs. Always check the AQI levels
+        before stepping out and remmeber to try and keep your air clean.</h2></section>\
+       <ul id="ways-to-prevent"> <h3>Ways to prevent air pollution</h3>
+       <li> Reduce the number of trips you take in your car. Walking and biking isn't so
+       bad, actually it's good for the environment's health and yours.</li>
+       <li> Keep your vechile's engines properly maintained at all times</li>
+       <li> Avoid buring Trash leaves or other materials</li> <br>
+       <img src="https://cff2.earth.com/uploads/2016/11/03135030/clean-air-pollution_1big_stokc.jpg" alt="unraveling clean air">`)
+    })
+}
 
 function watchForm() {
-    // barChart();
+    aboutUsButton();
     sliding();
     $('form').submit(event => {
         event.preventDefault()
